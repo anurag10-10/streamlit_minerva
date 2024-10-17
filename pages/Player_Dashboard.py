817fc5968496a00546a22b9b27a04a9d7a91b7b1
Anurag Tale <60420776+anurag10-10@users.noreply.github.com>
2024-10-17 15:17:35 +0530

    Update Player_Dashboard.py

--- pages/Player_Dashboard.py
+++ pages/Player_Dashboard.py
@@ -20,6 +20,201 @@ df2 = pd.read_csv('Minerva_vs_Sudeva_Sudeva_data.csv')
 team_name1 = df1['Team'].iloc[0]
 team_name2 = df2['Team'].iloc[0]
 
+##########################
+# STATS AND RATINGS #
+##########################
+st.title(f"Stats for {selected_player}")
+
+# Access the Google Drive link from Streamlit secrets
+url = st.secrets["url"]
+
+# Path where the model will be saved in the Streamlit Cloud environment
+model_path = 'player_ratings_prediction.pickle'
+
+# Function to download the model if not already downloaded
+def download_model(url, output_path):
+    if not os.path.exists(output_path):  # Check if model already exists
+        gdown.download(url, output_path, quiet=True, fuzzy=True, use_cookies=False)
+
+# Path where the model will be saved in the Streamlit Cloud environment
+model_path = 'player_ratings_prediction.pickle'
+
+# Download the model if it doesn't already exist
+download_model(st.secrets["url"], model_path)
+
+# Check if the file exists and has a reasonable size
+if os.path.exists(model_path) and os.path.getsize(model_path) > 1_000_000:  # Check for files larger than 1MB
+    try:
+        with open(model_path, 'rb') as f:
+            model = pickle.load(f)
+    except pickle.UnpicklingError:
+        st.error("Error loading the model. The file might be corrupted.")
+else:
+    st.error("Model download failed or the file is too small.")
+
+# Load the model using pickle
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
+# Load the model
+#model = pickle.load(open('player_ratings_prediction.pickle', 'rb'))
+
+# Load the columns from the JSON file
+with open('model_columns.json', 'r') as file:
+    model_columns = json.load(file)
+
+# Load event data for both teams (replace with actual file paths)
+df_team1 = pd.read_csv('Minerva_vs_Sudeva_Minerva_data.csv')
+df_team2 = pd.read_csv('Minerva_vs_Sudeva_Sudeva_data.csv')
+
+df1 = main_minerva.process_data(df_team1)
+df2 = main_minerva.process_data(df_team2)
+
+
+
+mins_df1 = main_minerva.mins_secs(df1)
+mins_df2 = main_minerva.mins_secs(df2)
+
+team1 = main_minerva.add_passer_recipient_columns(mins_df1)
+team2 = main_minerva.add_passer_recipient_columns(mins_df2)
+
+# Calculate goals for each team
+team1_goals = main_minerva.calculate_goals(df_team1)
+team2_goals = main_minerva.calculate_goals(df_team2)
+
+# Assign win/loss based on goals
+if team1_goals > team2_goals:
+    df_team1['win'] = 1
+    df_team1['lost'] = 0
+    df_team2['win'] = 0
+    df_team2['lost'] = 1
+elif team2_goals > team1_goals:
+    df_team1['win'] = 0
+    df_team1['lost'] = 1
+    df_team2['win'] = 1
+    df_team2['lost'] = 0
+else:
+    # In case of a draw
+    df_team1['win'] = 0
+    df_team1['lost'] = 0
+    df_team2['win'] = 0
+    df_team2['lost'] = 0
+
+
+player_stats1 = main_minerva.calculate_player_stats(df1)
+player_stats2 = main_minerva.calculate_player_stats(df2)
+
+player_stats1['is_home_team'] = 1 # home team
+player_stats2['is_home_team'] = 0 # away team
+
+# Calculate goals for each team
+team1_goals = main_minerva.calculate_goals(df_team1)
+team2_goals = main_minerva.calculate_goals(df_team2)
+
+# Assign win/loss based on goals
+if team1_goals > team2_goals:
+    player_stats1['win'] = 1
+    player_stats1['lost'] = 0
+    player_stats2['win'] = 0
+    player_stats2['lost'] = 1
+elif team2_goals > team1_goals:
+    player_stats1['win'] = 0
+    player_stats1['lost'] = 1
+    player_stats2['win'] = 1
+    player_stats2['lost'] = 0
+else:
+    # In case of a draw
+    player_stats1['win'] = 0
+    player_stats1['lost'] = 0
+    player_stats2['win'] = 0
+    player_stats2['lost'] = 0
+
+network_metrics_df1 = main_minerva.calculate_network_metrics(team1)
+network_metrics_df2 = main_minerva.calculate_network_metrics(team2)
+
+player_net_metrics1 = main_minerva.unique_jerseys(team1, network_metrics_df1)
+player_net_metrics2 = main_minerva.unique_jerseys(team2, network_metrics_df2)
+
+#st.write(print(type(player_net_metrics1)))
+#st.write(print(type(player_stats1)))
+
+def final_df(network_stats_df, player_stats_df):
+    final_df = network_stats_df.merge(player_stats_df, on='Player', how='left')
+    final_df.drop(columns =['Jersey'])
+    final_df = final_df[['Player','goals', 'assists', 'shots_ontarget',
+       'shots_offtarget', 'shotsblocked', 'drib_success', 'drib_unsuccess',
+       'keypasses', 'touches', 'passes_acc', 'passes_inacc', 'lballs_acc',
+       'lballs_inacc', 'grduels_w', 'grduels_l', 'aerials_w', 'aerials_l',
+       'fouls', 'clearances', 'interceptions', 'tackles', 'dribbled_past',
+       'tballs_acc', 'tballs_inacc', 'ycards', 'rcards', 'offsides',
+       'saved_pen', 'missed_penalties', 'owngoals', 'degree_centrality',
+       'betweenness_centrality', 'closeness_centrality', 'flow_centrality',
+       'flow_success', 'betweenness2goals', 'win', 'lost', 'is_home_team',
+       'minutesPlayed', 'game_duration', 'crosses_att', 'saves_made',
+       'goals_conceded']]
+    return final_df
+
+input_df1 = final_df(player_net_metrics1, player_stats1)
+input_df2 = final_df(player_net_metrics2, player_stats2)
+
+
+# Combine both teams' player statistics into one DataFrame
+player_stats = pd.concat([player_stats1, player_stats2])
+
+
+# Assuming df_team1 and df_team2 have the 'Team' column that contains the team names
+#team_name1 = df_team1['Team'].iloc[0]
+#team_name2 = df_team2['Team'].iloc[0]
+
+# Sidebar to select team
+#team_option = st.selectbox('Select Team', [team_name1, team_name2])
+
+# Filter players based on selected team
+if team == team_name1:
+    team_data = input_df1[input_df1['is_home_team'] == 1]
+    # Show only players from team_name1
+    #player_option = st.selectbox('Select a player', input_df1['Player'].sort_values().unique())
+else:
+    team_data = input_df2[input_df2['is_home_team'] == 0]
+    # Show only players from team_name2
+    #player_option = st.selectbox('Select a player', input_df2['Player'].sort_values().unique())
+
+
+# Combine both teams' player statistics into one DataFrame
+player_stats = pd.concat([player_stats1, player_stats2])
+
+input_df = pd.concat([input_df1, input_df2])
+
+# Assuming 'Player' is in input_df, filter data for the selected player
+player_data = input_df[input_df['Player'] == selected_player]
+
+# Drop the 'Player' column (since the model doesn't need this for prediction)
+player_data_for_prediction = player_data.drop(columns=['Player'])
+
+# Predict player rating using the model
+predicted_rating = model.predict(player_data_for_prediction)
+
+# Display the predicted rating
+#st.write(f"### Rating for {player_option}: {predicted_rating[0]:.2f}")
+minutes_played = player_data.loc[player_data['Player'] == selected_player, 'minutesPlayed'].values[0]
+accurate_passes = player_data.loc[player_data['Player'] == selected_player, 'passes_acc'].values[0]
+inaccurate_passes = player_data.loc[player_data['Player'] == selected_player, 'passes_inacc'].values[0]
+shots_on_target = player_data.loc[player_data['Player'] == selected_player, 'shots_ontarget'].values[0]
+shots_off_target = player_data.loc[player_data['Player'] == selected_player, 'shots_offtarget'].values[0]
+shots_blocked = player_data.loc[player_data['Player'] == selected_player, 'shotsblocked'].values[0]
+
+passing_accuracy = round((accurate_passes / (inaccurate_passes + accurate_passes)) * 100)
+#st.write(f"**Goals**: {inaccurate_passes}")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    ui.metric_card(title="Player Rating", content=f'{predicted_rating[0]:.2f}', description=f"{selected_player}", key="card1")
+with col2:
+    ui.metric_card(title="Passing Stats", content=f'{passing_accuracy}%', description=f"Total Passes: {inaccurate_passes + accurate_passes}", key="card2")
+with col3:
+    ui.metric_card(title="Total Shots", content=f"{shots_on_target + shots_off_target +shots_blocked}", description=f'Off Target: {shots_off_target}', key="card3")
+
+
 shotmap_df1 = df1[df1['Event'] == 'Shot']
 shotmap_df2 = df2[df2['Event'] == 'Shot']
 
@@ -226,200 +421,6 @@ with col2:
     shots_map(shotmap_df[shotmap_df['Player'] == selected_player], color, ax2)
     ax2.set_title(f'Total Shots: {total_shots}', fontsize=20)  # Show total shots in the plot title
     st.pyplot(fig2)
-
-##########################
-# STATS AND RATINGS #
-##########################
-st.title(f"Stats for {selected_player}")
-
-# Access the Google Drive link from Streamlit secrets
-url = st.secrets["url"]
-
-# Path where the model will be saved in the Streamlit Cloud environment
-model_path = 'player_ratings_prediction.pickle'
-
-# Function to download the model if not already downloaded
-def download_model(url, output_path):
-    if not os.path.exists(output_path):  # Check if model already exists
-        gdown.download(url, output_path, quiet=True, fuzzy=True, use_cookies=False)
-
-# Path where the model will be saved in the Streamlit Cloud environment
-model_path = 'player_ratings_prediction.pickle'
-
-# Download the model if it doesn't already exist
-download_model(st.secrets["url"], model_path)
-
-# Check if the file exists and has a reasonable size
-if os.path.exists(model_path) and os.path.getsize(model_path) > 1_000_000:  # Check for files larger than 1MB
-    try:
-        with open(model_path, 'rb') as f:
-            model = pickle.load(f)
-    except pickle.UnpicklingError:
-        st.error("Error loading the model. The file might be corrupted.")
-else:
-    st.error("Model download failed or the file is too small.")
-
-# Load the model using pickle
-with open(model_path, 'rb') as f:
-    model = pickle.load(f)
-# Load the model
-#model = pickle.load(open('player_ratings_prediction.pickle', 'rb'))
-
-# Load the columns from the JSON file
-with open('model_columns.json', 'r') as file:
-    model_columns = json.load(file)
-
-# Load event data for both teams (replace with actual file paths)
-df_team1 = pd.read_csv('Minerva_vs_Sudeva_Minerva_data.csv')
-df_team2 = pd.read_csv('Minerva_vs_Sudeva_Sudeva_data.csv')
-
-df1 = main_minerva.process_data(df_team1)
-df2 = main_minerva.process_data(df_team2)
-
-
-
-mins_df1 = main_minerva.mins_secs(df1)
-mins_df2 = main_minerva.mins_secs(df2)
-
-team1 = main_minerva.add_passer_recipient_columns(mins_df1)
-team2 = main_minerva.add_passer_recipient_columns(mins_df2)
-
-# Calculate goals for each team
-team1_goals = main_minerva.calculate_goals(df_team1)
-team2_goals = main_minerva.calculate_goals(df_team2)
-
-# Assign win/loss based on goals
-if team1_goals > team2_goals:
-    df_team1['win'] = 1
-    df_team1['lost'] = 0
-    df_team2['win'] = 0
-    df_team2['lost'] = 1
-elif team2_goals > team1_goals:
-    df_team1['win'] = 0
-    df_team1['lost'] = 1
-    df_team2['win'] = 1
-    df_team2['lost'] = 0
-else:
-    # In case of a draw
-    df_team1['win'] = 0
-    df_team1['lost'] = 0
-    df_team2['win'] = 0
-    df_team2['lost'] = 0
-
-
-player_stats1 = main_minerva.calculate_player_stats(df1)
-player_stats2 = main_minerva.calculate_player_stats(df2)
-
-player_stats1['is_home_team'] = 1 # home team
-player_stats2['is_home_team'] = 0 # away team
-
-# Calculate goals for each team
-team1_goals = main_minerva.calculate_goals(df_team1)
-team2_goals = main_minerva.calculate_goals(df_team2)
-
-# Assign win/loss based on goals
-if team1_goals > team2_goals:
-    player_stats1['win'] = 1
-    player_stats1['lost'] = 0
-    player_stats2['win'] = 0
-    player_stats2['lost'] = 1
-elif team2_goals > team1_goals:
-    player_stats1['win'] = 0
-    player_stats1['lost'] = 1
-    player_stats2['win'] = 1
-    player_stats2['lost'] = 0
-else:
-    # In case of a draw
-    player_stats1['win'] = 0
-    player_stats1['lost'] = 0
-    player_stats2['win'] = 0
-    player_stats2['lost'] = 0
-
-network_metrics_df1 = main_minerva.calculate_network_metrics(team1)
-network_metrics_df2 = main_minerva.calculate_network_metrics(team2)
-
-player_net_metrics1 = main_minerva.unique_jerseys(team1, network_metrics_df1)
-player_net_metrics2 = main_minerva.unique_jerseys(team2, network_metrics_df2)
-
-#st.write(print(type(player_net_metrics1)))
-#st.write(print(type(player_stats1)))
-
-def final_df(network_stats_df, player_stats_df):
-    final_df = network_stats_df.merge(player_stats_df, on='Player', how='left')
-    final_df.drop(columns =['Jersey'])
-    final_df = final_df[['Player','goals', 'assists', 'shots_ontarget',
-       'shots_offtarget', 'shotsblocked', 'drib_success', 'drib_unsuccess',
-       'keypasses', 'touches', 'passes_acc', 'passes_inacc', 'lballs_acc',
-       'lballs_inacc', 'grduels_w', 'grduels_l', 'aerials_w', 'aerials_l',
-       'fouls', 'clearances', 'interceptions', 'tackles', 'dribbled_past',
-       'tballs_acc', 'tballs_inacc', 'ycards', 'rcards', 'offsides',
-       'saved_pen', 'missed_penalties', 'owngoals', 'degree_centrality',
-       'betweenness_centrality', 'closeness_centrality', 'flow_centrality',
-       'flow_success', 'betweenness2goals', 'win', 'lost', 'is_home_team',
-       'minutesPlayed', 'game_duration', 'crosses_att', 'saves_made',
-       'goals_conceded']]
-    return final_df
-
-input_df1 = final_df(player_net_metrics1, player_stats1)
-input_df2 = final_df(player_net_metrics2, player_stats2)
-
-
-# Combine both teams' player statistics into one DataFrame
-player_stats = pd.concat([player_stats1, player_stats2])
-
-
-# Assuming df_team1 and df_team2 have the 'Team' column that contains the team names
-#team_name1 = df_team1['Team'].iloc[0]
-#team_name2 = df_team2['Team'].iloc[0]
-
-# Sidebar to select team
-#team_option = st.selectbox('Select Team', [team_name1, team_name2])
-
-# Filter players based on selected team
-if team == team_name1:
-    team_data = input_df1[input_df1['is_home_team'] == 1]
-    # Show only players from team_name1
-    #player_option = st.selectbox('Select a player', input_df1['Player'].sort_values().unique())
-else:
-    team_data = input_df2[input_df2['is_home_team'] == 0]
-    # Show only players from team_name2
-    #player_option = st.selectbox('Select a player', input_df2['Player'].sort_values().unique())
-
-
-# Combine both teams' player statistics into one DataFrame
-player_stats = pd.concat([player_stats1, player_stats2])
-
-input_df = pd.concat([input_df1, input_df2])
-
-# Assuming 'Player' is in input_df, filter data for the selected player
-player_data = input_df[input_df['Player'] == selected_player]
-
-# Drop the 'Player' column (since the model doesn't need this for prediction)
-player_data_for_prediction = player_data.drop(columns=['Player'])
-
-# Predict player rating using the model
-predicted_rating = model.predict(player_data_for_prediction)
-
-# Display the predicted rating
-#st.write(f"### Rating for {player_option}: {predicted_rating[0]:.2f}")
-minutes_played = player_data.loc[player_data['Player'] == selected_player, 'minutesPlayed'].values[0]
-accurate_passes = player_data.loc[player_data['Player'] == selected_player, 'passes_acc'].values[0]
-inaccurate_passes = player_data.loc[player_data['Player'] == selected_player, 'passes_inacc'].values[0]
-shots_on_target = player_data.loc[player_data['Player'] == selected_player, 'shots_ontarget'].values[0]
-shots_off_target = player_data.loc[player_data['Player'] == selected_player, 'shots_offtarget'].values[0]
-shots_blocked = player_data.loc[player_data['Player'] == selected_player, 'shotsblocked'].values[0]
-
-passing_accuracy = round((accurate_passes / (inaccurate_passes + accurate_passes)) * 100)
-#st.write(f"**Goals**: {inaccurate_passes}")
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    ui.metric_card(title="Player Rating", content=f'{predicted_rating[0]:.2f}', description=f"{selected_player}", key="card1")
-with col2:
-    ui.metric_card(title="Passing Stats", content=f'{passing_accuracy}%', description=f"Total Passes: {inaccurate_passes + accurate_passes}", key="card2")
-with col3:
-    ui.metric_card(title="Total Shots", content=f"{shots_on_target + shots_off_target +shots_blocked}", description=f'Off Target: {shots_off_target}', key="card3")
 
 col1, col2 = st.columns(2)
 
